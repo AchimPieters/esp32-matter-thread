88ebd708aafdf5be9f4a72f15ca1620ba1c57e3f
Achim Pieters <Achim.Pieters@outlook.com>
2026-04-27 19:36:51 +0200

    Fix IDF 5.4 dependency graph and mark IDF 6 as experimental

--- components/json/json_shim.c
+++
@@ -1,7 +0,0 @@
-/*
- * ESP-IDF v6 removed the legacy component name "json".
- * Some esp_matter versions still require a component called "json".
- *
- * This shim restores that component name and forwards to built-in `cjson`.
- */
-void esp32_matter_thread_json_component_shim(void) {}
